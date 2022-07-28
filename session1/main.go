8312744d1641954295b2f91d88c762caf5975d45
lehuyloc007 <79748205+lehuyloc007@users.noreply.github.com>
2022-07-28 17:31:30 +0700

    add env

--- session1/main.go
+++ session1/main.go
@@ -1,8 +1,26 @@
 //cmd: "go work init" tạo file go.work để làm việc với nhiều project trong cùng 1 thư mục
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("hello")
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatalln("Error loading .env file")
+	}
+	dsn := os.Getenv("DB_CONNECTION")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln("Cannot connect to MySQL:", err)
+	}
+
+	log.Println("Connected:", db)
 }
